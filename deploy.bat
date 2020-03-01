617f4d503fb1ce252cca64c12da418a8010a7c5c
yukito yamamotosearch-ms:displayname=検索場所%3A%20ダウンロード <yukito_yamamoto@change-jp.com>
2020-03-01 13:46:05 +0900

    first commit

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,1 @@
-git init && git add . && git commit -m "first commit" && git remote add origin git@github.com:yukiyukiponsu/MergeCSVFile.git && git push -u origin master
+git init && git add . && git commit -m "first commit" && git remote rm origin && git remote add origin git@github.com:yukiyukiponsu/MergeCSVFile.git && git push -u origin master
